a6e474493a52a32f98e684b24aa960246f042ee3
Your Name <you@example.com>
2026-01-04 13:18:58 +0000

    Clean up

--- internal/scanner/patterns.go
+++ internal/scanner/patterns.go
@@ -14,25 +14,25 @@ func buildDefaultPatterns() []CommentPattern {
 	patterns := make([]CommentPattern, 0, len(markers)*4)
 
 	for _, marker := range markers {
-		// Pattern 1: // TODO: description (C-style single line)
+		// Pattern 1:(C-style single line)
 		patterns = append(patterns, CommentPattern{
 			Type:    marker,
 			Pattern: regexp.MustCompile(`//\s*` + marker + `[:\s]+(.+)`),
 		})
 
-		// Pattern 2: # TODO: description (Shell-style)
+		// Pattern 2:(Shell-style)
 		patterns = append(patterns, CommentPattern{
 			Type:    marker,
 			Pattern: regexp.MustCompile(`#\s*` + marker + `[:\s]+(.+)`),
 		})
 
-		// Pattern 3: /* TODO: description */ or /* TODO: description (C-style block)
+		// Pattern 3:(C-style block)
 		patterns = append(patterns, CommentPattern{
 			Type:    marker,
 			Pattern: regexp.MustCompile(`/\*\s*` + marker + `[:\s]+(.+?)(?:\s*\*/|$)`),
 		})
 
-		// Pattern 4: <!-- TODO: description --> (HTML/XML)
+		// Pattern 4:(HTML/XML)
 		patterns = append(patterns, CommentPattern{
 			Type:    marker,
 			Pattern: regexp.MustCompile(`<!--\s*` + marker + `[:\s]+(.+?)\s*-->`),
